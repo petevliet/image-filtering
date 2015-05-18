9c5d883b4dcb09032266066948138cc94b1a250f
Peter Vliet <petervliet@gmail.com>
2015-05-18 11:15:27 -0600

    filter by price xboxes

--- app.js
+++ app.js
@@ -13,18 +13,72 @@ $(document).ready(function() {
 		$('body').css('background-size', 'cover');
 	});
 
-	$(".low").on("click", function(){
+	$(".low").on("click", function(e){
+		e.preventDefault();
 		var houses = $(".image-container");
 		$(houses).find(".home").sort(function(a, b){
 			return $(a).data('price') - $(b).data('price');
 		}).appendTo(houses);
 	});
 
-	$(".high").on("click", function(){
+	$(".high").on("click", function(e){
+		e.preventDefault();
 		var houses = $(".image-container");
 		$(houses).find(".home").sort(function(a, b){
 			return $(b).data('price') - $(a).data('price');
 		}).appendTo(houses);
 	});
+
+	$("#1").on("click", function(){
+		$(".home").filter(function(){
+			return $(this).data('price') > 50000;
+		}).remove();
+	});
+
+	$("#2").on("click", function(){
+		$(".home").filter(function(){
+			return $(this).data('price') > 100000;
+		}).remove();
+		$(".home").filter(function(){
+			return $(this).data('price') < 50000;
+		}).remove();
+	});
+
+	$("#3").on("click", function(){
+		$(".home").filter(function(){
+			return $(this).data('price') > 200000;
+		}).remove();
+		$(".home").filter(function(){
+			return $(this).data('price') < 100000;
+		}).remove();
+	});
+
+	$("#4").on("click", function(){
+		$(".home").filter(function(){
+			return $(this).data('price') > 300000;
+		}).remove();
+		$(".home").filter(function(){
+			return $(this).data('price') < 200000;
+		}).remove();
+	});
+
+	$("#5").on("click", function(){
+		$(".home").filter(function(){
+			return $(this).data('price') > 500000;
+		}).remove();
+		$(".home").filter(function(){
+			return $(this).data('price') < 300000;
+		}).remove();
+	});
+
+	$("#6").on("click", function(){
+		$(".home").filter(function(){
+			return $(this).data('price') > 1000000;
+		}).remove();
+		$(".home").filter(function(){
+			return $(this).data('price') < 500000;
+		}).remove();
+	});
+
 
 });
