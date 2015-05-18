dc25843816b88bdb6044a445aa46e8da527f5cfa
Peter Vliet <petervliet@gmail.com>
2015-05-18 09:52:09 -0600

    light circle click makes like bg

--- app.js
+++ app.js
@@ -2,5 +2,10 @@ $(document).ready(function() {
 	console.log("The document is ready!");
 	$(".dark").on("click", function(){
 		$('body').css('background', 'url(img/dark_wood.png)');
-	})
-})
+	});
+
+	$(".light").on("click", function(){
+		$('body').css('background', 'url(img/tileable_wood_texture.png)');
+	});
+
+});
